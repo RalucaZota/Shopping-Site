313d099ebc290b74dd140fbf3fb19a6289e9f61f
RalucaZota <zotaraluca649@gmail.com>
2024-01-02 18:43:07 +0200

    created different components for billing information and done validation

---
+++ client/src/scenes/checkout/Shipping.jsx
@@ -0,0 +1,67 @@
+// @ts-nocheck
+import { Box, Checkbox, FormControlLabel, Typography } from "@mui/material";
+import AddressForm from "./AddressForm";
+
+const Shipping = ({
+  values,
+  errors,
+  touched,
+  handleBlur,
+  handleChange,
+  setFieldValue,
+}) => {
+  return (
+    <Box m="30px auto">
+      {/* Billing form */}
+      <Box>
+        <Typography sx={{ mb: "15px" }} fontSize="18px">
+          Billing information
+        </Typography>
+        <AddressForm
+          type="billingAddress"
+          values={values.billingAddress}
+          errors={errors}
+          touched={touched}
+          handleBlur={handleBlur}
+          handleChange={handleChange}
+        />
+      </Box>
+      <Box mb="20px">
+        <FormControlLabel
+          label="Same for Shipping Address"
+          control={
+            <Checkbox
+              defaultChecked
+              value={values.shippingAddress.isSameAddress}
+              onChange={() =>
+                setFieldValue(
+                  "shippingAddress.isSameAddress",
+                  !values.shippingAddress.isSameAddress
+                )
+              }
+            />
+          }
+        />
+      </Box>
+      {/* Shipping form */}
+      {!values.shippingAddress.isSameAddress && (
+        <Box>
+          <Typography sx={{ mb: "15px" }} fontSize="18px">
+            Shipping information
+          </Typography>
+          <AddressForm
+            type="shippingAddress"
+            values={values.shippingAddress}
+            errors={errors}
+            touched={touched}
+            handleBlur={handleBlur}
+            handleChange={handleChange}
+          />
+        <div>test</div>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default Shipping;
